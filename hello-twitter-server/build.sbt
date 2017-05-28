1de7701632987888f4653f95460645ac4227d587
Kazuhiro Sera <seratch@gmail.com>
2017-05-28 17:35:09 +0900

    Copied the first example from the project's readme page
      sbt run # compile and start a server process
      curl -v localhost:8889/foo # try the API server
      open http://localhost:9990/ # open the admin pages

---
+++ hello-twitter-server/build.sbt
@@ -0,0 +1,2 @@
+scalaVersion := "2.12.2"
+libraryDependencies += "com.twitter" %% "twitter-server" % "1.29.0"
---
+++ hello-twitter-server/src/main/scala/example/BasicServer.scala
@@ -0,0 +1,22 @@
+import com.twitter.finagle.{Http, Service}
+import com.twitter.finagle.http.{Request, Response, Status}
+import com.twitter.server.TwitterServer
+import com.twitter.util.{Await, Future}
+
+object BasicServer extends TwitterServer {
+  val service = new Service[Request, Response] {
+    def apply(request: Request) = {
+      val response = Response(request.version, Status.Ok)
+      response.contentString = "hello"
+      Future.value(response)
+    }
+  }
+
+  def main(): Unit = {
+    val server = Http.serve(":8889", service)
+    onExit {
+      server.close()
+    }
+    Await.ready(server)
+  }
+}
